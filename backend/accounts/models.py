965224c6e186348b68a63b38b8dbc72fe1ee7da3
wesleywil <wesleywilsonti@gmail.com>
2023-11-14 13:05:11 -0300

    added User model and some basic configurations

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from PIL import Image
 
-# Create your models here.
+
+class User(AbstractUser):
+    picture = models.ImageField(
+        upload_to='profiles/', default='profiles/default.jpg')
+
+    def __srt__(self):
+        return "username: " + self.username
+
+    def save(self, *args, **kwargs):
+        super().save()
+        img = Image.open(self.profile_picture.path)
+        if img.height > 150 or img.width > 150:
+            new_img = (150, 150)
+            img.thumbnail(new_img)
+            img.save(self.profile_picture.path)
