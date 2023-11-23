30831333a3a6cf9cd69938a91608274c0a42b8a7
wesleywil <wesleywilsonti@gmail.com>
2023-11-23 11:51:01 -0300

    added views and url's for the CardType functions

--- backend/cards/urls.py
+++ backend/cards/urls.py
@@ -4,6 +4,10 @@ from .views import (
     CardPostView,
     CardDetailsView,
     CardDetailsUpdateView,
+    CardsTypeView,
+    CardTypePostView,
+    CardTypeDetailsView,
+    CardTypeDetailsUpdateView,
     TransactionViewSet,
     TransactionDetailsViewSet,
 
@@ -12,10 +16,19 @@ from .views import (
 app_name = "cards_apis"
 
 urlpatterns = [
+    # Cards
     path('', CardsView.as_view(), name='list_cards'),
     path('', CardPostView.as_view(), name='create_card'),
     path('<int:pk>/', CardDetailsView.as_view(), name='details_card'),
     path('<int:pk>', CardDetailsUpdateView.as_view(), name='update_delete_card'),
+    # Card Types
+    path('types/', CardsTypeView.as_view(), name='list_card_types'),
+    path('types/', CardTypePostView.as_view(), name='create_card_types'),
+    path('types/<int:pk>/', CardTypeDetailsView.as_view(),
+         name='details_card_types'),
+    path('types/<int:pk>/', CardTypeDetailsUpdateView.as_view(),
+         name='update_delete_card_types'),
+    # Transactions
     path('transactions/', TransactionViewSet.as_view(), name='transactions'),
     path('transactions/<int:pk>/', TransactionDetailsViewSet.as_view(),
          name='details_transactions')
--- backend/cards/views.py
+++ backend/cards/views.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from knox.auth import TokenAuthentication
 
 
-from .models import Card, Transaction
+from .models import Card, CardType, Transaction
 from .serializers import (
     CardSerializer,
+    CardTypeSerializer,
     TransactionSerializer,
     TransactionViewerSerializer
 )
@@ -72,6 +73,68 @@ class CardDetailsUpdateView(APIView):
     def delete(self, request, pk, format=None):
         card = self.get_object(pk)
         card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CardsTypeView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, format=None):
+        queryset = CardType.objects.all().order_by('-title')
+        serializer = CardTypeSerializer(
+            queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class CardTypePostView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    authentication_classes = (TokenAuthentication,)
+
+    def post(self, request, format=None):
+        serializer = CardTypeSerializer(
+            data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CardTypeDetailsView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get_object(self, pk):
+        try:
+            return CardType.objects.get(pk=pk)
+        except CardType.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        cardtype = self.get_object(pk)
+        serializer = CardTypeSerializer(cardtype, context={"request": request})
+        return Response(serializer.data)
+
+
+class CardTypeDetailsUpdateView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+    authentication_classes = (TokenAuthentication)
+
+    def get_object(self, pk):
+        try:
+            return CardType.objects.get(pk=pk)
+        except CardType.DoesNotExist:
+            return Http404
+
+    def put(self, request, pk, format=None):
+        cardtype = self.get_object(pk)
+        serializer = CardTypeSerializer(cardtype, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        cardtype = self.get_object(pk)
+        cardtype.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
