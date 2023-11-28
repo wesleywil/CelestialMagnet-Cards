15edb7105acbb6356e467c9ae674489aaa3e7bcf
wesleywil <wesleywilsonti@gmail.com>
2023-11-28 12:42:35 -0300

    updated menu component by including a link to the cards page

--- frontend/src/components/menu/menu.component.tsx
+++ frontend/src/components/menu/menu.component.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import { FaBars } from "react-icons/fa";
 
 const Menu = () => {
   return (
-    <div className="drawer">
+    <div className="drawer z-40">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="p-2 drawer-content">
         {/* Page content here */}
@@ -19,15 +20,22 @@ const Menu = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <div className="menu px-4 py-8 w-80 min-h-full flex flex-col items-center bg-base-200 text-base-content">
           {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
+          <Link href="/">
+            <img
+              src="http://dummyimage.com/100x100"
+              alt="logo"
+              className="rounded-full w-24 h-24 p-2 border border-[#fcfcfa] hover:border-[#7bc6a2] transform duration-500 ease-in-out"
+            />
+          </Link>
+          <Link
+            href="/cards"
+            className="mt-8 text-xl text-[#fcfcfa] hover:text-[#7bc6a2] font-semibold transform duration-500 ease-in-out"
+          >
+            Cards
+          </Link>
+        </div>
       </div>
     </div>
   );
