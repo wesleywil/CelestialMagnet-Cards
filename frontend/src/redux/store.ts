a27e8432229ab821d9d9ef0700dbdd19711e0010
wesleywil <wesleywilsonti@gmail.com>
2023-11-29 12:23:54 -0300

    added UserReducer that will handle user states, reducer functions and mainly the createAsyncThunk functions

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cardReducer from "./cards/cards";
+import userReducer from "./user/user";
 import utilReducer from "./utils/utils";
 
 export const store = configureStore({
     reducer:{
+        user:userReducer,
         utils:utilReducer,
         cards:cardReducer
     }
