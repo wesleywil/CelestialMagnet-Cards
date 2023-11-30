be0c5a1612a88276397208735720d88a2929d493
wesleywil <wesleywilsonti@gmail.com>
2023-11-30 13:47:57 -0300

    updated cardSlicer by including a createAsyncThunk to fetch user cards and selectCardById reducer function

--- frontend/src/redux/cards/cards.tsx
+++ frontend/src/redux/cards/cards.tsx
@@ -1,6 +1,7 @@
 import { Card } from "@/utils/interfaces";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { headers } from "../user/user";
 
 export interface CardState {
   card: Card;
@@ -31,6 +32,22 @@ export const fetchCards = createAsyncThunk("cards/fetchCards", async () => {
   return cards;
 });
 
+export const fetchUserCards = createAsyncThunk(
+  "cards/fetchUserCards",
+  async () => {
+    const res = await fetch(`${url}/usercards/`, {
+      method: "GET",
+      headers: headers,
+      credentials: "include",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch user cards");
+    }
+    const userCards: Card[] = await res.json();
+    return userCards;
+  }
+);
+
 export const cardSlice = createSlice({
   name: "cards",
   initialState,
@@ -40,6 +57,12 @@ export const cardSlice = createSlice({
         (item) => item.name === action.payload
       );
       state.group_cards = selectedCards !== undefined ? selectedCards : [];
+    },
+    selectCardById: (state, action: PayloadAction<number>) => {
+      const selectedCard = state.cards.find(
+        (item) => item.id === action.payload
+      );
+      state.card = selectedCard !== undefined ? selectedCard : ({} as Card);
     },
     selectCard: (state, action: PayloadAction<string>) => {
       const selectedCard = state.group_cards.find(
@@ -82,12 +105,28 @@ export const cardSlice = createSlice({
       .addCase(fetchCards.rejected, (state, action: PayloadAction<any>) => {
         state.status = "error while trying to retrieve cards";
         state.error = String(action.payload);
+      })
+      .addCase(fetchUserCards.pending, (state) => {
+        state.status = "trying to fetch user cards";
+      })
+      .addCase(
+        fetchUserCards.fulfilled,
+        (state, action: PayloadAction<Card[] | []>) => {
+          state.status = "user cards retrieved successfully";
+          state.cards = action.payload;
+          state.filtered_cards = action.payload;
+        }
+      )
+      .addCase(fetchUserCards.rejected, (state, action: PayloadAction<any>) => {
+        state.status = "error while trying to retrieve user cards";
+        state.error = String(action.payload);
       });
   },
 });
 
 export const {
   selectGroupCards,
+  selectCardById,
   selectCard,
   resetGroupCards,
   filterByCardType,
