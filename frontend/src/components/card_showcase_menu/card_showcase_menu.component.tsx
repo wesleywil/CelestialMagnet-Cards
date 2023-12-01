a830ac49d0a823b982a3798272c57f9704cf245a
wesleywil <wesleywilsonti@gmail.com>
2023-12-01 12:15:20 -0300

    updated CaseShowcaseMenu by changing the tier buttons to something more simplistic

--- frontend/src/components/card_showcase_menu/card_showcase_menu.component.tsx
+++ frontend/src/components/card_showcase_menu/card_showcase_menu.component.tsx
@@ -1,35 +1,34 @@
 import { useDispatch } from "react-redux";
-import { FaGem } from "react-icons/fa";
 import type { AppDispatch } from "@/redux/store";
 import { selectCard } from "@/redux/cards/cards";
 
 const CardShowcaseMenu = () => {
   const dispatch = useDispatch<AppDispatch>();
   return (
-    <div className="h-12 my-2 flex justify-center gap-2 text-3xl">
+    <div className="h-10 flex justify-center gap-2 text-xs font-bold">
       <button
         onClick={() => dispatch(selectCard("bronze"))}
-        className="p-2 flex justify-center items-center bg-amber-950 hover:bg-amber-800 rounded-full transform duration-500 ease-in-out"
+        className="px-2 flex justify-center items-center text-white bg-amber-950 hover:bg-amber-800 rounded transform duration-500 ease-in-out"
       >
-        <FaGem />
+        Bronze
       </button>
       <button
         onClick={() => dispatch(selectCard("silver"))}
-        className="p-2 flex justify-center items-center bg-slate-400 hover:bg-slate-200 rounded-full transform duration-500 ease-in-out"
+        className="px-2 flex justify-center items-center text-white hover:text-slate-800 bg-slate-400 hover:bg-slate-200 rounded transform duration-500 ease-in-out"
       >
-        <FaGem />
+        Silver
       </button>
       <button
         onClick={() => dispatch(selectCard("golden"))}
-        className="p-2 flex justify-center items-center bg-amber-500 hover:bg-amber-300 rounded-full transform duration-500 ease-in-out"
+        className="px-2 flex justify-center items-center text-white hover:text-amber-800 bg-amber-500 hover:bg-amber-300 rounded transform duration-500 ease-in-out"
       >
-        <FaGem />
+        Golden
       </button>
       <button
         onClick={() => dispatch(selectCard("black_diamond"))}
-        className="p-2 flex justify-center items-center bg-black hover:bg-slate-700 border rounded-full transform duration-500 ease-in-out"
+        className="px-2 flex justify-center items-center text-white bg-black hover:bg-slate-700 border rounded transform duration-500 ease-in-out"
       >
-        <FaGem />
+        Diamond
       </button>
     </div>
   );
