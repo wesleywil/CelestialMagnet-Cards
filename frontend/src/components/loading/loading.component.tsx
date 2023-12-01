91b072d1c4a4ce03afec0e069067cc36762acee9
wesleywil <wesleywilsonti@gmail.com>
2023-12-01 13:27:28 -0300

    updated loading and not_authenticated components by changing the style and loading animation

--- frontend/src/components/loading/loading.component.tsx
+++ frontend/src/components/loading/loading.component.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Loading = () => {
+  const url = "http://localhost:8000/images/card_bases";
+  const [images] = useState([
+    `${url}/Inferno_Dragon.jpg`,
+    `${url}/Pyro_Phoenix.jpg`,
+    `${url}/Molten_Golem.jpg`,
+    `${url}/Flame_Salamander.jpg`,
+    `${url}/Ember_Elemental.jpg`,
+  ]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
   return (
-    <div className="flex flex-col justify-center items-center text-[#7bc6a2]">
-      <div className="w-32 h-40 bg-[#7bc6a2] rounded animate_loading"></div>
-      <h1 className="text-2xl animate-pulse">Loading</h1>
-    </div>
+    <main className="p-24 flex min-h-screen flex-col items-center justify-center text-[#fcfcfa] z-0">
+      <div className="flex flex-col gap-4 items-center">
+        <img
+          src={images[currentImageIndex]}
+          alt="card"
+          className="w-60 rounded animate_loading"
+        />
+        <h1 className="text-2xl text-[#f3c842] animate-pulse">Loading</h1>
+      </div>
+    </main>
   );
 };
 
