e908ee2e7ab54e1fbdd06f0f71ba2267cb927769
wesleywil <wesleywilsonti@gmail.com>
2023-12-11 13:10:13 -0300

    updated Menu component, a completly change in the menu component making more elegant

--- frontend/src/components/menu/menu.component.tsx
+++ frontend/src/components/menu/menu.component.tsx
@@ -1,45 +1,66 @@
-import Link from "next/link";
-import { FaBars } from "react-icons/fa";
+"use client";
 
+import Link from "next/link";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 const Menu = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div className="drawer z-40">
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-      <div className="p-2 drawer-content">
-        {/* Page content here */}
-        <label
-          htmlFor="my-drawer"
-          className="text-4xl text-[#7bc6a2] drawer-button"
-        >
-          <FaBars />
-        </label>
-      </div>
-      <div className="drawer-side">
-        <label
-          htmlFor="my-drawer"
-          aria-label="close sidebar"
-          className="drawer-overlay"
-        ></label>
-        <div className="menu px-4 py-8 w-80 min-h-full flex flex-col items-center bg-base-200 text-base-content">
-          {/* Sidebar content here */}
-          <Link href="/">
-            <img
-              src="http://dummyimage.com/100x100"
-              alt="logo"
-              className="rounded-full w-24 h-24 p-2 border border-[#fcfcfa] hover:border-[#7bc6a2] transform duration-500 ease-in-out"
-            />
-          </Link>
+    <div className="absolute w-full flex flex-col justify-center z-50">
+      <button
+        onClick={() => setShowMenu(!showMenu)}
+        className="self-center my-2 text-[#7bc6a2] hover:text-[#fcfcfa] text-3xl transform duration-500 ease-in-out"
+      >
+        <FaBars />
+      </button>
+      <div
+        style={
+          showMenu
+            ? { height: "27rem", border: "1px solid #7bc6a2" }
+            : { height: "0" }
+        }
+        className="w-1/2 mx-auto flex flex-col items-center gap-2 bg-[#262c35] rounded overflow-hidden shadow-sm transform duration-700 ease-in-out"
+      >
+        <Link href="/">
+          <img
+            src="http://localhost:8000/images/card_bases/Tidal_Leviathan.jpg"
+            alt="logo"
+            className="rounded-full w-24 h-24 mt-8 p-2 border border-[#fcfcfa] hover:border-[#7bc6a2] transform duration-500 ease-in-out"
+          />
+        </Link>
+        <div className="w-full flex flex-col items-center gap-2 font-bold text-[#7bc6a2]  text-2xl">
           <Link
             href="/cards"
-            className="mt-8 text-xl text-[#fcfcfa] hover:text-[#7bc6a2] font-semibold transform duration-500 ease-in-out"
+            className="px-2 py-1 hover:text-[#fcfcfa] transform duration-500 ease-in"
           >
             Cards
           </Link>
           <Link
+            href="/transactions"
+            className="px-2 py-1 text-2xl hover:text-[#fcfcfa] transform duration-500 ease-in"
+          >
+            Transactions
+          </Link>
+        </div>
+        <div className="w-full mt-4 pt-2 flex flex-col items-center gap-2 font-bold text-[#7bc6a2] text-2xl border-t border-[#fcfcfa]">
+          <Link
             href="/mycards"
-            className="mt-8 text-xl text-[#fcfcfa] hover:text-[#7bc6a2] font-semibold transform duration-500 ease-in-out"
+            className="px-2 py-1 hover:text-[#fcfcfa] transform duration-500 ease-in"
           >
             My Cards
+          </Link>
+          <Link
+            href="/mytransactions"
+            className="px-2 py-1 text-2xl hover:text-[#fcfcfa] transform duration-500 ease-in"
+          >
+            My Transactions
+          </Link>
+          <Link
+            href="/"
+            className="mt-4 px-2 py-1 text-xl text-[#fcfcfa] hover:text-[#7bc6a2] bg-[#7bc6a2] hover:bg-[#fcfcfa] rounded transform duration-500 ease-in"
+          >
+            Sign Out
           </Link>
         </div>
       </div>
