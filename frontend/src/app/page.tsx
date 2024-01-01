8a4246d0ce91e2e8bfe89919c7acd2ef2b56dec2
wesleywil <wesleywilsonti@gmail.com>
2024-01-01 16:44:52 -0300

    updated homepage by adding some animations using framer-motion

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,11 @@
-import { Bebas_Neue, Barlow_Condensed } from "next/font/google";
+"use client";
+
+import { Bebas_Neue } from "next/font/google";
 
 // Components
 import HomeShowcase from "@/components/home_showcase/home_showcase.component";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const bebasNeue = Bebas_Neue({ weight: "400", subsets: ["latin"] });
 
@@ -15,25 +18,47 @@ export default function Home() {
       <div className="w-full flex md:flex-col-reverse xl:flex-row gap-2 justify-center md:text-center xl:text-left">
         <div className="xl:w-1/3 self-center text-[#e6eeee]">
           <div className="flex flex-col">
-            <div className="md:self-center xl:self-start w-20 px-1 my-1 flex flex-col justify-center">
+            <motion.div
+              animate={{
+                translateY: [-20, 0],
+                transition: { duration: 0.5, ease: "easeIn" },
+              }}
+              className="md:self-center xl:self-start w-20 px-1 my-1 flex flex-col justify-center"
+            >
               <span className="px-1 text-center text-[#e6eeee] font-semibold">
                 001 / 100
               </span>
               <div className=" h-1 bg-[#e6eeee]"></div>
-            </div>
-            <h1
+            </motion.div>
+            <motion.h1
+              animate={{
+                translateY: [-50, 0],
+                transition: { duration: 0.5, ease: "easeIn" },
+              }}
               className={`${bebasNeue.className} my-2 text-6xl text-[#e05f5f] font-semibold`}
             >
               CelestialMagnet Cards
-            </h1>
-            <h1 className="px-1 text-2xl ">
+            </motion.h1>
+            <motion.h1
+              animate={{
+                translateY: [-50, 0],
+                transition: { duration: 0.55, ease: "easeIn" },
+              }}
+              className="px-1 text-2xl "
+            >
               Unleash the Power of Cosmic Trades!
-            </h1>
+            </motion.h1>
           </div>
 
-          <h2 className="my-2 px-1 text-sm text-gray-300 ">
+          <motion.h2
+            animate={{
+              translateY: [-50, 0],
+              transition: { duration: 0.6, ease: "easeIn" },
+            }}
+            className="my-2 px-1 text-sm text-gray-300 "
+          >
             Explore, Trade, and Collect Rare Cards in the Celestial Universe
-          </h2>
+          </motion.h2>
           <Link
             href="/cards"
             className="my-4 px-1 text-[#e05f5f] hover:text-[#e6eeee] transform duration-500 ease-in-out"
